0cd07da44581e50e0bcf3fd40dc810d1b0180336
Omar Mohammad <omohammad@kudu.com.sa>
2026-09-05 10:00:56 +0300

    Support On Hand Quantity

--- src/Endpoints/Inventories/CurrentInventoryEndpoint.php
+++ src/Endpoints/Inventories/CurrentInventoryEndpoint.php
@@ -19,41 +19,47 @@ class CurrentInventoryEndpoint
         $this->client = new CrunchTimeClient(self::SIGNATURE);
     }
 
-    /**
-     * Get all CrunchTime Current Inventory data.
-     *
-     * @param array<string, mixed> $query Additional query parameters.
-     * @return array
-     */
+
     public function get(array $query = []): array
     {
         return $this->client->get(self::ENDPOINT, $query);
     }
 
 
-    /**
-     * Get all Current Inventory data for a specific location.
-     *
-     * @param string $store_code The store code.
-     * @return array
-     */
     public function getForLocation(string $store_code, array $query = []): array
     {
         if (!is_string($store_code)) {
             throw new InvalidArgumentException('Invalid store code format. Expected a string.');
         }
 
-        return $this->client->get(self::ENDPOINT, ['locationCode' => $store_code, ...$query]);
-//        return InventoryData::collection($this->client->get(self::ENDPOINT, ['inventoryDate' => $target_date, 'locationCode' => $store_code, 'includeNull' => false]), $target_date, $store_code);
+        $currentInventoryDetails = [];
+
+        $products = $this->client->get(self::ENDPOINT, ['locationCode' => $store_code, 'pageSize' => 100, ...$query]);
+
+        $currentInventoryDetails = array_merge($currentInventoryDetails, $products['currentInventoryDetails']['currentInventoryDetailDetails']);
+
+        if ($products['hasNext']) {
+            $page_number = 2;
+            while ($products['hasNext']) {
+                $products = $this->getForLocationByPageNumber($page_number, $store_code, $query);
+                $currentInventoryDetails = array_merge($currentInventoryDetails, $products['currentInventoryDetails']['currentInventoryDetailDetails']);
+                $page_number++;
+            }
+        }
+
+        return $currentInventoryDetails;
     }
 
-    /**
-     * Get all Current Inventory data for a specific location and specific product.
-     *
-     * @param string $product_number The product number.
-     * @param string $store_code The store code.
-     * @return array
-     */
+    public function getForLocationByPageNumber(string $page_number, string $store_code, array $query = []): array
+    {
+        if (!is_string($store_code)) {
+            throw new InvalidArgumentException('Invalid store code format. Expected a string.');
+        }
+
+        return $this->client->get(self::ENDPOINT, ['locationCode' => $store_code, 'pageSize' => 100, 'pageNumber' => $page_number, ...$query]);
+    }
+
+
     public function getForLocationAndProduct(string $product_number, string $store_code, array $query = []): array
     {
         if (!is_string($store_code)) {
@@ -64,8 +70,8 @@ class CurrentInventoryEndpoint
             throw new InvalidArgumentException('Invalid product number format. Expected a string.');
         }
 
-        return $this->client->get(self::ENDPOINT, ['locationCode' => $store_code, 'productNumber' => $product_number, ...$query]);
-//        return InventoryData::collection($this->client->get(self::ENDPOINT, ['inventoryDate' => $target_date, 'locationCode' => $store_code, 'includeNull' => false]), $target_date, $store_code);
+        return $this->client->get(self::ENDPOINT, ['locationCode' => $store_code, 'productNumber' => $product_number, 'pageSize' => 100, ...$query]);
     }
+
 
 }
